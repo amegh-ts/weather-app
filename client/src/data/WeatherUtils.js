6529fc6faaf4d8e27d0ff6a515336ceec5ada1d6
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-03-13 03:40:55 +0530

    13-03-2024

--- client/src/data/WeatherUtils.js
+++ client/src/data/WeatherUtils.js
@@ -1,3 +1,4 @@
 export const weatherIcon = (weather) => weather?.weatherData?.weather[0]?.icon;
 export const temperature = (weather) => Math.floor(weather?.weatherData?.main?.temp);
 export const feelsLike = (weather) => Math.floor(weather?.weatherData?.main?.feels_like);
+export const cityName=(forecast)=>forecast?.weatherData?.city?.name;
--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Hero.scss'
-import { feelsLike, temperature, weatherIcon } from '../data/WeatherUtils';
+import { cityName, feelsLike, temperature, weatherIcon } from '../data/WeatherUtils';
 
 const Hero = ({ weather, forecast }) => {
   const icon = weatherIcon(weather);
   const temp = temperature(weather);
   const feels = feelsLike(weather);
+  const city = cityName(forecast)
 
 
   const list = forecast?.weatherData?.list;
 
   console.log('list', list);
+
+  const firstSix = list?.slice(0, 6);
+
+  console.log('list', firstSix);
 
   // Function to extract the first reading of weather from each day
   function getFirstWeatherReadingPerDay(list) {
@@ -37,8 +42,6 @@ const Hero = ({ weather, forecast }) => {
   // Get first weather reading from each day
   const firstWeatherReadings = getFirstWeatherReadingPerDay(list);
 
-  // Output the result
-  console.log(firstWeatherReadings);
 
   return (
     <div className='Hero'>
@@ -55,10 +58,30 @@ const Hero = ({ weather, forecast }) => {
             <img src={`https://openweathermap.org/img/wn/${icon}@4x.png`} alt="Weather Icon" />
           </div>
         </div>
+
+        <div className="middle">
+          <h4>TODAY'S FORECAST</h4>
+          <div className="container">
+            {firstSix?.map((day, index) => (
+            <div className="column" key={index}>
+              <span>{new Date(day.dt * 1000).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })}</span>
+              <span>
+                <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} alt="Weather Icon" />
+                {day.weather[0].description}
+              </span>
+            </div>
+          ))}
+          </div>
+        </div>
+
+        <div className="bottom">
+        <h4>AIR CONDITION</h4>
+
+        </div>
       </div>
 
       <div className="right">
-        <h3>7 day forecast</h3>
+        <h3>6 day forecast for {city}</h3>
         <div className="container">
           {firstWeatherReadings?.map((day, index) => (
             <div className="row" key={index}>
