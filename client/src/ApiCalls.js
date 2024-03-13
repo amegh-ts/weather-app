95bfd325b354e8599a5f2e4bc891994b1d5cbe4c
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-03-13 06:17:40 +0530

    Update ApiCalls.js

--- client/src/ApiCalls.js
+++ client/src/ApiCalls.js
@@ -1,8 +1,6 @@
 import { publicRequest } from "./RequestMethods";
 
 export const weatherData = async (data) => {
-    console.log('coordinates',data); 
-
     try {
        const res=await publicRequest.post('/fetchweather',data)
        console.log('Response Status:', res.status);
@@ -11,21 +9,16 @@ export const weatherData = async (data) => {
     } catch (error) {
         console.log(error);
     //    console.log(error.response.data);
-
     }
 }
 
 export const forecastData = async (data) => {
-    console.log('coordinates',data); 
-
     try {
        const res=await publicRequest.post('/forecast',data)
        console.log('Response Status:', res.status);
        return res.data;
-
     } catch (error) {
         console.log(error);
     //    console.log(error.response.data);
-
     }
 }
