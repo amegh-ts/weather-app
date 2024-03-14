daee2030975053cd745a5cb218899f6eb0968d3d
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-03-14 22:06:50 +0530

    14-03-2024

--- client/src/components/Weekly.jsx
+++ client/src/components/Weekly.jsx
@@ -4,7 +4,7 @@ const Weekly = ({day}) => {
     return (
         <div className="row" >
             <span>{new Date(day.dt * 1000).toLocaleDateString()}</span>
-            <span>
+            <span className='rfid'>
                 <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png`} alt="Weather Icon" />
                 {day.weather[0].main}
             </span>
